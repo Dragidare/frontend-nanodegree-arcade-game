29d41268fffd73af49a9cd12ef4a5c18abb118cb
dragidare <antic.darko@gmail.com>
2016-11-21 21:34:45 +0100

    gemms and score added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if(this.x > 505) {
+      this.x = 0;
+    }
+
+    checkCollision(this);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +34,52 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x,y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/char-boy.png'
+}
+// update() method
+Player.prototype.update = function() {
 
+  if(this.x > 505) {
+    this.x = 505;
+  }
+
+};
+// render() method
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// handleInput() method
+Player.prototype.handleInput = function(keyPressed) {
+  if (keyPressed == 'left') {
+    player.x -= player.speed;
+  }
+  if (keyPressed == 'right') {
+    player.x += player.speed;
+  }
+  if (keyPressed == 'up') {
+    player.y -= player.speed;
+  }
+  if (keyPressed == 'down') {
+    player.y += player.speed;
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies =[];
+var player = new Player (202,385,50);
+
+for (var i = 0; i <= 2; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+        allEnemies.push(enemy);
+    }
 
 
 
@@ -44,3 +95,94 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+var checkCollision = function(anEnemy) {
+    // check for collision between enemy and player
+    if (
+        player.y + 131 >= anEnemy.y + 90
+        && player.x + 25 <= anEnemy.x + 88
+        && player.y + 73 <= anEnemy.y + 135
+        && player.x + 76 >= anEnemy.x + 11) {
+        console.log('collided');
+        player.x = 202.5;
+        player.y = 383;
+    }
+
+    // check for player reaching top of canvas and winning the game
+    // if player wins, add 1 to the score and level
+    // pass score as an argument to the increaseDifficulty function
+    if (player.y + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        console.log('you made it!');
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+
+        score += 1;
+        gameLevel += 1;
+        console.log('current score: ' + score + ', current level: ' + gameLevel);
+        increaseDifficulty(score);
+
+    }
+
+    // check if player runs into left, bottom, or right canvas walls
+    // prevent player from moving beyond canvas wall boundaries
+    if (player.y > 383 ) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+};
+
+//Gems our player can collect
+var Gem = function(){
+this.x = this.x = 0 + 101 * Math.floor(5 * Math.random());
+this.y = this.y = 62 + 85.5 * Math.floor(3 * Math.random());
+this.sprite = 'images/GemOrange.png';
+};
+
+
+// render() method
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// update() method
+Gem.prototype.update = function() {
+
+  if(this.x > 505) {
+    this.x = 505;
+  }
+    checkCollect(this);
+
+};
+
+var gem = new Gem ();
+var score = 0;
+var gameLevel = 1;
+
+var checkCollect = function(anGem) {
+    // check for collision between enemy and player
+    if (
+        player.y + 131 >= anGem.y + 90
+        && player.x + 25 <= anGem.x + 88
+        && player.y + 73 <= anGem.y + 135
+        && player.x + 76 >= anGem.x + 11) {
+        console.log('collected');
+        score += 10;
+        console.log('current score: ' + score + ', current level: ' + gameLevel);
+        gem.x = 0 + 101 * Math.floor(5 * Math.random());
+        gem.y = 62 + 85.5 * Math.floor(3 * Math.random());
+      };
+
+};
+
+
+console.log(allEnemies);
+console.log(gem);
--- js/engine.js
+++ js/engine.js
@@ -95,6 +95,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        gem.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -152,6 +153,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -171,6 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+        'images/GemOrange.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
