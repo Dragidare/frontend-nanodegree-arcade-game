69d74ae13855ca07a7c8f841489c14b83e68ad0d
dragidare <antic.darko@gmail.com>
2016-11-27 23:36:14 +0100

    add, lifes, display score and restart

--- js/app.js
+++ js/app.js
@@ -17,18 +17,23 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
+    if (playerLives > 0) {
+    this.x += this.speed * dt * gameLevel;
 
     if(this.x > 505) {
       this.x = 0;
     }
 
     checkCollision(this);
+      };
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    displayScoreLevel(score, gameLevel, playerLives);
+
 };
 
 // Now write your own player class
@@ -50,7 +55,9 @@ Player.prototype.update = function() {
 };
 // render() method
 Player.prototype.render = function() {
+  if (playerLives > 0) {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // handleInput() method
@@ -67,16 +74,22 @@ Player.prototype.handleInput = function(keyPressed) {
   if (keyPressed == 'down') {
     player.y += player.speed;
   }
+  if (keyPressed == 'space' && playerLives == 0) {
+    score = 0;
+    playerLives = 10;
+    gameLevel = 1;
+  }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
 var allEnemies =[];
 var player = new Player (202,385,50);
 
 for (var i = 0; i <= 2; i++) {
-        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 125);
 
         allEnemies.push(enemy);
     }
@@ -90,7 +103,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        32: 'space'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
@@ -104,7 +118,8 @@ var checkCollision = function(anEnemy) {
         && player.x + 25 <= anEnemy.x + 88
         && player.y + 73 <= anEnemy.y + 135
         && player.x + 76 >= anEnemy.x + 11) {
-        console.log('collided');
+        playerLives -= 1;
+        console.log('collided =' + playerLives);
         player.x = 202.5;
         player.y = 383;
     }
@@ -165,24 +180,35 @@ Gem.prototype.update = function() {
 
 var gem = new Gem ();
 var score = 0;
+var playerLives = 10;
 var gameLevel = 1;
+var scoreLevelDiv = document.createElement('h3');
 
 var checkCollect = function(anGem) {
-    // check for collision between enemy and player
+    // check if playe collect a gem
     if (
         player.y + 131 >= anGem.y + 90
         && player.x + 25 <= anGem.x + 88
         && player.y + 73 <= anGem.y + 135
         && player.x + 76 >= anGem.x + 11) {
         console.log('collected');
-        score += 10;
-        console.log('current score: ' + score + ', current level: ' + gameLevel);
+        score += (10*gameLevel);
+        console.log('current score: ' + score + ', current level: ' + gameLevel + ' lives: ' + playerLives);
         gem.x = 0 + 101 * Math.floor(5 * Math.random());
         gem.y = 62 + 85.5 * Math.floor(3 * Math.random());
       };
 
 };
+// Function to display player's score
+var displayScoreLevel = function(aScore, aLevel, aLives) {
+    var canvas = document.getElementsByTagName('canvas');
+    var firstCanvasTag = canvas[0];
 
+    // add player score lives and level to div element
+    scoreLevelDiv.innerHTML = 'Score: ' + aScore
+        + ' / ' + 'Level: ' + aLevel + ' / ' + 'Lives: ' + aLives;
+    document.body.appendChild(scoreLevelDiv, firstCanvasTag[0]);
+};
 
 console.log(allEnemies);
 console.log(gem);
