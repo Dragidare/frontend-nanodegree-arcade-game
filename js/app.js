38c930526ed1e3965d8ac60ffb6054274aac74c6
Darko Antic <antic.darko@gmail.com>
2016-12-05 15:01:09 +0100

    objects are implemented using JS OOP programming features

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// DOM element for score display
+var scoreLevelDiv = document.createElement('h3');
 // 'constant' variables as sugessted in the code review
 var titleWidth = 101,
     titleHeight = 83;
@@ -8,10 +10,14 @@ var Chraracter = function(x, y) {
     this.y = y;
 };
 
+var Game = function(score, lives, level) {
+    this.score = score;
+    this.playerLives = lives;
+    this.gameLevel = level;
+};
 
 // Enemies our player must avoid
 Enemy.prototype = new Chraracter;
-
 function Enemy(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -23,22 +29,16 @@ function Enemy(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (playerLives > 0) {
-        this.x += this.speed * dt * gameLevel;
-
-        if (this.x > 505) {
-            this.x = 0;
-        }
-
+    this.checkCollisions();
+    if (game.playerLives > 0) {
+        this.x += this.speed * dt * game.gameLevel;
     };
-
 
     // check for player reaching top of canvas and winning the game
     // if player wins, add 1 to the score and level
@@ -46,31 +46,18 @@ Enemy.prototype.update = function(dt) {
 
 };
 
-Player.prototype.checkCollisions = function(){
-    if (
-        player.y + 131 >= this.y + 90 &&
-        player.x + 25 <= this.x + 88 &&
-        player.y + 73 <= this.y + 135 &&
-        player.x + 76 >= this.x + 11) {
-        playerLives -= 1;
-        console.log('collided =' + playerLives);
+Enemy.prototype.checkCollisions = function() {
+    if (player.y + 131 >= this.y + 90 && player.x + 25 <= this.x + 88 && player.y + 73 <= this.y + 135 && player.x + 76 >= this.x + 11) {
+        game.playerLives -= 1;
+        console.log('collided =' + game.playerLives);
         player.x = 202.5;
         player.y = 383;
     }
-};
-
-
-
-// Draw the character on the screen, required method for game
-Chraracter.prototype.render = function() {
-
-    if (playerLives > 0) {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        displayScoreLevel(score, gameLevel, playerLives);
+    if (this.x > 505) {
+        this.x = 0;
     }
 
 };
-
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -81,8 +68,9 @@ function Player(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png'
-}
-// update() method
+};
+
+// player update method
 Player.prototype.update = function() {
 
     if (this.x > 505) {
@@ -106,17 +94,13 @@ Player.prototype.update = function() {
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, 505, 171);
 
-        score += 1;
-        gameLevel += 1;
-        console.log('current score: ' + score + ', current level: ' + gameLevel);
-
-
+        game.score += 1;
+        game.gameLevel += 1;
+        console.log('current score: ' + game.score + ', current level: ' + game.gameLevel);
     }
-
 };
-// render() method
 
-// handleInput() method
+// player handleinput method
 Player.prototype.handleInput = function(keyPressed) {
     if (keyPressed == 'left') {
         this.x -= titleWidth;
@@ -130,27 +114,73 @@ Player.prototype.handleInput = function(keyPressed) {
     if (keyPressed == 'down') {
         this.y += titleHeight;
     }
-    if (keyPressed == 'space' && playerLives == 0) {
-        score = 0;
-        playerLives = 10;
-        gameLevel = 1;
+    if (keyPressed == 'space' && game.playerLives == 0) {
+        game.score = 0;
+        game.playerLives = 10;
+        game.gameLevel = 1;
     }
 }
+
+//Gems our player can collect
+Gem.prototype = new Chraracter;
+
+function Gem(x, y) {
+    this.x = this.x = 0 + titleWidth * Math.floor(5 * Math.random());
+    this.y = this.y = 62 + titleHeight * Math.floor(3 * Math.random());
+    this.sprite = 'images/GemOrange.png';
+};
+
+// gem update() method
+Gem.prototype.update = function() {
+
+    if (this.x > 505) {
+        this.x = 505;
+    }
+    if (player.y + 131 >= this.y + 90 && player.x + 25 <= this.x + 88 && player.y + 73 <= this.y + 135 && player.x + 76 >= this.x + 11) {
+        console.log('collected');
+        game.score += (10 * game.gameLevel);
+        console.log('current score: ' + game.score + ', current level: ' + game.gameLevel + ' lives: ' + game.playerLives);
+        gem.x = 0 + titleWidth * Math.floor(5 * Math.random());
+        gem.y = 62 + titleHeight * Math.floor(3 * Math.random());
+    };
+
+};
+
+// Function to display player's score
+Game.prototype.displayScoreLevel = function(aScore, aLevel, aLives) {
+    var canvas = document.getElementsByTagName('canvas');
+    var firstCanvasTag = canvas[0];
+
+    // add player score lives and level to div element
+    scoreLevelDiv.innerHTML = 'Score: ' + aScore + ' / ' + 'Level: ' + aLevel + ' / ' + 'Lives: ' + aLives;
+    document.body.appendChild(scoreLevelDiv, firstCanvasTag[0]);
+};
+
+Game.prototype.update = function() {
+    this.displayScoreLevel(this.score, this.gameLevel, this.playerLives);
+}
+
+// Draw the characters on the screen, required method for game
+Chraracter.prototype.render = function() {
+
+    if (game.playerLives > 0) {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var game = new Game(0, 10, 1);
+var player = new Player(202, 410);
+var gem = new Gem();
+
 var allEnemies = [];
-var player = new Player(202, 385);
-
 for (var i = 0; i <= 2; i++) {
-    var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 125);
-
+    var enemy = new Enemy(0, 62 + titleHeight * Math.floor(3 * Math.random()), Math.random() * 125);
     allEnemies.push(enemy);
 }
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -162,59 +192,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down',
         32: 'space'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-//Gems our player can collect
-Gem.prototype = new Chraracter;
-
-function Gem(x, y) {
-    this.x = this.x = 0 + titleWidth * Math.floor(5 * Math.random());
-    this.y = this.y = 62 + titleHeight * Math.floor(3 * Math.random());
-    this.sprite = 'images/GemOrange.png';
-};
-
-
-// update() method
-
-Gem.prototype.update = function() {
-
-    if (this.x > 505) {
-        this.x = 505;
-    }
-    if (
-        player.y + 131 >= this.y + 90 &&
-        player.x + 25 <= this.x + 88 &&
-        player.y + 73 <= this.y + 135 &&
-        player.x + 76 >= this.x + 11) {
-        console.log('collected');
-        score += (10 * gameLevel);
-        console.log('current score: ' + score + ', current level: ' + gameLevel + ' lives: ' + playerLives);
-        gem.x = 0 + titleWidth * Math.floor(5 * Math.random());
-        gem.y = 62 + titleHeight * Math.floor(3 * Math.random());
-    };
-
-};
-
-var gem = new Gem();
-var score = 0;
-var playerLives = 10;
-var gameLevel = 1;
-var scoreLevelDiv = document.createElement('h3');
-
-
-// Function to display player's score
-var displayScoreLevel = function(aScore, aLevel, aLives) {
-    var canvas = document.getElementsByTagName('canvas');
-    var firstCanvasTag = canvas[0];
-
-    // add player score lives and level to div element
-    scoreLevelDiv.innerHTML = 'Score: ' + aScore +
-        ' / ' + 'Level: ' + aLevel + ' / ' + 'Lives: ' + aLives;
-    document.body.appendChild(scoreLevelDiv, firstCanvasTag[0]);
-};
-
-console.log(allEnemies);
-console.log(gem);
